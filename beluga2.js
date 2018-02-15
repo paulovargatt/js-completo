313212a80d5bf4785ef342b0b1db9ec30760a52a
Paulo Vargatt <pvargatt@gmail.com>
2018-02-15 00:59:04 -0200

    desafio

--- beluga2.js
+++ beluga2.js
@@ -1,9 +1,30 @@
 var blg = (function () {
-    var blg = {};
+    var beluga = {};
 
-    blg.$ = document.querySelector.bind(document);
-    blg.$$ = document.querySelectorAll.bind(document);
+    beluga.$ = document.querySelector.bind(document);
+    beluga.$$ = document.querySelectorAll.bind(document);
 
-    return blg;
+    Array.prototype.getMedia = function(){
+      return _calcularMedia.apply(this,this);
+      //return _calcularMedia(...this); //ES6
+    };
+
+
+
+    function _calcularMedia() {
+        let params = arguments;
+
+        let hasNaN = Array.prototype.some.call(params, (n) => isNaN(n));
+        if(hasNaN) throw new Error('somente numero!');
+
+        let total =  Array.prototype.reduce.call(params, function (total, atual, i) {
+            return total + parseFloat(atual);
+        });
+        return total / arguments.length;
+    }
+
+    beluga.calcularMedia = _calcularMedia;
+
+    return beluga;
 
 })();
